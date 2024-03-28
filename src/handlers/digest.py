306b69ccad0380cc4e9b3a2a9f13ea54cce1dd4b
sumtopmus <sumtopmus@users.noreply.github.com>
2024-03-27 21:01:53 -0400

    Added experimental `digest` feature
    - Available only for admins
    - So far only collect the data (no data analysis)

---
+++ src/handlers/digest.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from dynaconf import settings
+from telegram import Update
+from telegram.ext import CommandHandler, ContextTypes, filters, MessageHandler
+
+from utils import log
+
+
+def create_handlers() -> list:
+    """Creates handlers that process `digest` command."""
+    return [
+        CommandHandler('digest', digest, filters.User(username=settings.ADMINS)),
+        MessageHandler((filters.TEXT | filters.CAPTION) & ~filters.COMMAND & filters.Chat(settings.CHAT_ID), log_message),
+    ]
+
+
+async def log_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Logs message sent to the chat."""
+    log('log_message')
+    if update.message.text:
+        context.bot_data['messages'].append({
+            'name': update.effective_user.full_name,
+            'text': update.message.text
+        })
+    elif update.message.caption:
+        context.bot_data['messages'].append({
+            'name': update.effective_user.full_name,
+            'text': update.message.caption
+        })
+    
+
+async def digest(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Basic admin digest command."""
+    log('digest')
+    latest_digest = datetime.fromisoformat(context.bot_data['latest_digest']['timestamp'])
+    if (datetime.now() - latest_digest).total_seconds() < settings.SLOW_MODE:
+        await update.message.reply_text(
+            'Посмотри последний дайджест, он был не так давно.',
+            reply_to_message_id=context.bot_data['latest_digest']['message_id'])
+    else:
+        message = 'Дайджест чата:\n\n' + '\n'.join(f'{m['name']}: {m['text']}' for m in context.bot_data['messages'][-10:]) \
+        + '\n\n_#digest_'
+        bot_message = await update.message.reply_text(message)
+        context.bot_data['latest_digest']['timestamp'] = datetime.now().isoformat()
+        context.bot_data['latest_digest']['message_id'] = bot_message.message_id
+        context.bot_data['messages'] = []
+
+
+async def daily_digest(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send morning message and add a new timer."""
+    log('daily_digest')
+    message = 'Ежедневный дайджест:\n\n' + '\n'.join(context.bot_data['messages'][-10:]) + '\n\n_#digest #daily_'
+    await context.bot.sendMessage(chat_id=settings.DIGEST_CHAT_ID, text=message)
+    context.bot_data['messages'] = []
--- src/init.py
+++ src/init.py
@@ -1,16 +1,27 @@
+from datetime import datetime, time
+from dynaconf import settings
 from telegram.ext import Application
 
 import utils
 from handlers import error
 from handlers import debug, info
-from handlers import request, schedule, welcome
+from handlers import digest, request, schedule, welcome
 
 
 async def post_init(app: Application) -> None:
     """Initializes bot with data and its tasks."""
-    utils.log('intializing_players')
+    utils.log('intializing')
     app.bot_data.setdefault('players', {})    
-    app.bot_data.setdefault('schedule', {})    
+    app.bot_data.setdefault('schedule', {})
+    app.bot_data.setdefault('messages', [])
+    app.bot_data['messages'] = []
+    app.bot_data.setdefault('latest_digest', {})
+    app.bot_data['latest_digest'].setdefault('timestamp', datetime.now().isoformat())
+    app.bot_data['latest_digest'].setdefault('message_id', None)
+    app.job_queue.run_daily(
+        digest.daily_digest,
+        time.fromisoformat(settings.DAILY_DIGEST_TIME),
+        name='DAILY_DIGEST')
 
 
 def add_handlers(app: Application) -> None:
@@ -21,5 +32,5 @@ def add_handlers(app: Application) -> None:
     for module in [debug, info]:
         app.add_handlers(module.create_handlers())
     # General chat handling.
-    for module in [request, schedule, welcome]:
+    for module in [digest, request, schedule, welcome]:
         app.add_handlers(module.create_handlers())
